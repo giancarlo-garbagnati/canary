9f5f1e8119d58783fd5c6d0503635f73be9f4805
giancarlo-garbagnati <giancarlo.garbagnati@gmail.com>
2025-05-26 23:49:43 -0700

    adding init files

---
+++ main.py
@@ -0,0 +1,13 @@
+import utils.io_utils as utils
+
+def main():
+    print('test')
+    d = {
+        test:'test',
+        data:[1,2,3]
+    }
+    uri = 
+    utils.write_yaml(d, uri)
+
+if __name__ == "__main__":
+    main()
