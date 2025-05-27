4f5efc4bfd3061e9e9828c1ae9f17180dd773e16
giancarlo-garbagnati <giancarlo.garbagnati@gmail.com>
2025-05-27 14:54:18 -0700

    update config files, including email config and updating gitignore

---
+++ config/email_config_template.py
@@ -0,0 +1,8 @@
+# include an email_config.py file with your email access information
+
+EMAIL_FROM = 'you@example.com'
+EMAIL_TO = 'you@example.com'
+EMAIL_USER = 'you@example.com'
+EMAIL_PASS = 'your-email-password'
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 465
