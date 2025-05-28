2cfdbaed0a7b0515b01a06e3bfd14d0bf22cfeb0
giancarlo-garbagnati <giancarlo.garbagnati@gmail.com>
2025-05-28 00:46:57 -0700

    email utils update

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import utils.io_utils as utils
 def main():
     print('test')
     d = {
-        test:'test',
-        data:[1,2,3]
+        'test':'test',
+        'data':[1,2,3]
     }
     uri = ''
     utils.write_yaml(d, uri)
--- utils/email_utils.py
+++ utils/email_utils.py
@@ -22,8 +22,24 @@ def format_email_subject():
     )
     return subject
 
-def format_email_body():
-    pass
+def format_email_body(job_list):
+    html = "<h2>New Job Postings</h2><ul>"
+    for job in job_list:
+        html += f"<li><a href='{job['url']}'>{job['title']} at {job['company']}</a><br>"
+        html += f"<i>{job['location']} - Posted {job['posted_date']}</i><br>"
+        html += f"{job['description_snippet']}</li><br><br>"
+    html += "</ul>"
+    return html
 
 def send_email(subject, body):
-    pass
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = to_email
+
+    mime_html = MIMEText(html_content, 'html')
+    msg.attach(mime_html)
+
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+        server.login(login, password)
+        server.sendmail(from_email, to_email, msg.as_string())
+    
