28ddbe574f81540bfbd3bda9a6cf23fd0c653660
giancarlo-garbagnati <giancarlo.garbagnati@gmail.com>
2025-05-28 16:29:07 -0700

    short docstrings for emails_utils

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -6,7 +6,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-
 from_email = email_config.EMAIL_FROM
 to_email = email_config.EMAIL_TO
 smtp_server = email_config.SMTP_SERVER
@@ -14,8 +13,8 @@ smtp_port = email_config.SMTP_PORT
 login = email_config.EMAIL_USER
 password = email_config.EMAIL_PASS
 
-
 def format_email_subject():
+    """Formats the subject line for an email"""
     subject = (
         'Job Digest ' + str(date.today()) + ' ' + 
         str(datetime.now().hour) + ':' + str(datetime.now().minute)
@@ -23,6 +22,7 @@ def format_email_subject():
     return subject
 
 def format_email_body(job_list):
+    """Formats the body for the job posting digest"""
     html = "<h2>New Job Postings</h2><ul>"
     for job in job_list:
         html += f"<li><a href='{job['url']}'>{job['title']} at {job['company']}</a><br>"
@@ -32,6 +32,7 @@ def format_email_body(job_list):
     return html
 
 def send_email(subject, body):
+    """Sends an email"""
     msg = MIMEMultipart("alternative")
     msg['Subject'] = subject
     msg['From'] = from_email
